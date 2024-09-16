14c9e2310bc3025b6bb9d70626b8bda44725d4bc
Dmitry <metaldr@yandex.ru>
2024-09-16 19:21:49 +0300

    Public directory is configured via publicDir config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 import {resolve} from 'path';
 
 export default defineConfig({
+	publicDir: resolve(__dirname, 'public'), // the same as default value
 	build: {
 		assetsInlineLimit: 4096,
 	}
